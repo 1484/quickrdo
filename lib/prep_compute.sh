b16f2cc6046ad0fc25145f1ab82a812d6ef80ab0
Etsuji Nakai <enakai@redhat.com>
2015-05-01 11:40:18 +0900

    Fix: change repo info for havana EOL

--- lib/prep_compute.sh
+++ lib/prep_compute.sh
@@ -9,7 +9,9 @@ function pre_install {
     systemctl start iptables.service
     systemctl enable iptables.service
 
-    yum -y install http://repos.fedorapeople.org/repos/openstack/openstack-havana/rdo-release-havana-9.noarch.rpm
+    yum -y install http://repos.fedorapeople.org/repos/openstack/EOL/openstack-havana/rdo-release-havana-9.noarch.rpm
+    sed -i 's/openstack\/openstack-havana/openstack\/EOL\/openstack-havana/' /etc/yum.repos.d/rdo-release.repo
+    sed -i 's/$releasever/20/g' /etc/yum.repos.d/puppetlabs.repo
     yum -y install openstack-packstack
 }
 
--- setup.sh
+++ setup.sh
@@ -18,7 +18,9 @@ function rdo_install {
     rm -f /usr/lib/systemd/system/mariadb.service
     cp /usr/lib/systemd/system/mysqld.service /usr/lib/systemd/system/mariadb.service
 
-    yum -y install http://repos.fedorapeople.org/repos/openstack/openstack-havana/rdo-release-havana-9.noarch.rpm
+    yum -y install http://repos.fedorapeople.org/repos/openstack/EOL/openstack-havana/rdo-release-havana-9.noarch.rpm
+    sed -i 's/openstack\/openstack-havana/openstack\/EOL\/openstack-havana/' /etc/yum.repos.d/rdo-release.repo
+    sed -i 's/$releasever/20/g' /etc/yum.repos.d/puppetlabs.repo
     yum -y install openstack-packstack
 
     ./lib/genanswer.sh controller
