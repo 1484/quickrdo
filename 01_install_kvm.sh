5e714e2bfd63d941e6f50f453afab90150aa3204
Takeru Ishibashi <keruru@localhost.localdomain>
2021-05-02 23:49:58 -0400

    sudoでechoは出来ないので修正

--- 01_install_kvm.sh
+++ 01_install_kvm.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sudo dnf -y install virt-manager virt-viewer virt-install libguestfs-tools libvirt-daemon-config-network
-sudo echo "options kvm-intel nested=1" > /etc/modprobe.d/kvm-intel.conf
-sudo echo "options kvm-amd nested=1" > /etc/modprobe.d/kvm-amd.conf
+sudo sh -c "echo 'options kvm-intel nested=1' > /etc/modprobe.d/kvm-intel.conf"
+sudo sh -c "echo 'options kvm-amd nested=1' > /etc/modprobe.d/kvm-amd.conf"
 
